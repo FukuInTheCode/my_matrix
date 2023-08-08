d710ff8d32d3c0e07616e06793b21da14237ed20
FukuInTheCode <tomc.code@gmail.com>
2023-08-08 23:43:39 +0200

    rand bug fix

--- src/func/matrixRand.c
+++ src/func/matrixRand.c
@@ -15,12 +15,12 @@ void my_Matrix_RandFloat(double minN, double maxN, const unsigned int count, ...
     unsigned int i;
     for(i = 0; i<count; i++) {
         my_Matrix *A = va_arg(args, my_Matrix *);
-        __Rand(A, minN, maxN);
+        __RandFloat(A, minN, maxN);
     }
     va_end(args);
 }
 
-void __RandFloat(my_Matrix *A, int minN, int maxN) {
+void __RandInt(my_Matrix *A, int minN, int maxN) {
     unsigned int i, j;
     for(i = 0; i<A->m; i++) {
         for(j = 0; j<A->n; j++) {
@@ -35,7 +35,7 @@ void my_Matrix_RandInt(int minN, int maxN, const unsigned int count, ...) {
     unsigned int i;
     for(i = 0; i<count; i++) {
         my_Matrix *A = va_arg(args, my_Matrix *);
-        __Rand(A, minN, maxN);
+        __RandInt(A, minN, maxN);
     }
     va_end(args);
 }
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]) {
     my_Matrix result = {.m=0, .n=0};
 
     my_Matrix_Create(2, 2, 1, &A);
-    my_Matrix_Rand(0, 10, 1, &A);
+    my_Matrix_RandInt(0, 10, 1, &A);
     
     my_Matrix_Print(1, &A);
 
