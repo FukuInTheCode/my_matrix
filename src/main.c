d2d12a65d9392664d31cc57127d78c9cfffa73c4
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 20:33:12 +0200

    applyfunc funtion is now added

--- src/main.c
+++ src/main.c
@@ -1,23 +1,22 @@
 #include "../includes/my.h"
 
+double timetwo(double x) {
+    return x * 2;
+}
+
 int main(int argc, char* argv[]) {
     srand(time(NULL));
-
-    my_Matrix A = {.m = 0, .n = 0};
-    my_Matrix B = {.m = 0, .n = 0};
+    my_Matrix A = {.m=0, .n=0};
     my_Matrix result = {.m=0, .n=0};
 
     my_Matrix_Create(2, 2, 1, &A);
-    my_Matrix_RandInt(1, 5, 1, &A);
-    
-    my_Matrix_Inverse(&A, &B);
+    my_Matrix_RandInt(1, 10, 1, &A);
 
-    my_Matrix_Print(2, &A, &B);
+    my_Matrix_ApplyFunc(&A, timetwo, &result);
 
-    my_Matrix_Product(&result, 2, &A, &B);
+    my_Matrix_Print(2, &A, &result);
 
-    my_Matrix_Print(1, &result);
-    
-    my_Matrix_Free(3, &A, &result, &B);
+    my_Matrix_Free(2, &A, &result);
+
     return 0;
 }
