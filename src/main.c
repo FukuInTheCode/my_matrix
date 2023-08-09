877ce32b4a607ce3bb1051434e757024a30b627c
FukuInTheCode <tomc.code@gmail.com>
2023-08-09 19:18:08 +0200

    product was getting the transpose by itself...

--- src/main.c
+++ src/main.c
@@ -4,17 +4,20 @@ int main(int argc, char* argv[]) {
     srand(time(NULL));
 
     my_Matrix A = {.m = 0, .n = 0};
+    my_Matrix B = {.m = 0, .n = 0};
     my_Matrix result = {.m=0, .n=0};
 
     my_Matrix_Create(2, 2, 1, &A);
-    my_Matrix_RandInt(0, 10, 1, &A);
+    my_Matrix_RandInt(1, 5, 1, &A);
+    my_Matrix_Create(2, 2, 1, &B);
+    my_Matrix_RandInt(1, 5, 1, &B);
     
-    my_Matrix_Print(1, &A);
+    my_Matrix_Print(2, &A, &B);
 
-    my_Matrix_AddScalar(&A, 10, &A);
+    my_Matrix_Product(&result, 2, &A, &B);
 
-    my_Matrix_Print(1, &A);
+    my_Matrix_Print(1, &result);
     
-    my_Matrix_Free(2, &A, &result);
+    my_Matrix_Free(3, &A, &result, &B);
     return 0;
 }
