e2368b05aada73b45f663665d2da423891f71ad2
FukuInTheCode <tomc.code@gmail.com>
2023-08-11 18:24:41 +0200

    can now swap on column

--- src/main.c
+++ src/main.c
@@ -3,13 +3,17 @@
 int main(int argc, char* argv[]) {
     srand(time(NULL));
     my_Matrix A = {.m=0, .n=0};
-    my_Matrix result = {.m=0, .n=0};
     my_Matrix B = {.m=0, .n=0};
+    my_Matrix C = {.m=0, .n=0};
+    my_Matrix T = {.m=0, .n=0};
 
     my_Matrix_Create(2, 2, 1, &A);
-    my_Matrix_RandInt(1, -10, 1, &A);
+    my_Matrix_RandInt(1, 10, 1, &A);
 
-    my_Matrix_SwapRow(&A, 0, 1, &result);
+    my_Matrix_SwapCol(&A, 0, 1, &B);
+    my_Matrix_SwapRow(&B, 0, 1, &C);
+
+    my_Matrix_T(&A, &T);
 
     // my_Matrix_ConcatRow(&B, &A, &result);
 
@@ -17,11 +21,9 @@ int main(int argc, char* argv[]) {
 
     // my_Matrix_ConcatCol(&A, &B, &result);
 
-    my_Matrix_Print(2, &A, &result);
+    my_Matrix_Print(3, &A, &C, &T);
 
-    printf("%f, %f, %d", my_Matrix_Sum(&A), my_Matrix_Sum(&result), my_Matrix_Equals(&A, &B));
-
-    my_Matrix_Free(3, &A, &result, &B);
+    my_Matrix_Free(4, &A, &C, &B, &T);
 
     return 0;
 }
