f5ad323bf25ffd779bea430015ce6334e1b38dd6
FukuInTheCode <tomc.code@gmail.com>
2023-08-16 00:20:49 +0200

    copy cs

--- src/func/matrixCopy.c
+++ src/func/matrixCopy.c
@@ -1,12 +1,12 @@
 #include "../../includes/my.h"
 
-void my_matrix_copy(my_matrix* A, my_matrix* copy) {
-    my_matrix_create(A->m, A->n, 1, copy);
+void my_matrix_copy(my_matrix* A, my_matrix* copy)
+{
     unsigned int i, j;
-    for(i = 0; i<A->m; i++) {
-        for(j=0; j<A->n; j++) {
+    my_matrix_create(A->m, A->n, 1, copy);
+    for(i = 0; i < A->m; i++) {
+        for(j = 0; j < A->n; j++) {
             my_matrix_set(copy, j, i, A->arr[i][j]);
         }
     }
 }
-
