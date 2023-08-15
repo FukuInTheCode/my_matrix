2a53ffa0a8677b087e122d5cd9c050eca7691c6b
FukuInTheCode <tomc.code@gmail.com>
2023-08-16 00:30:14 +0200

    fed

--- src/func/matrix_create.c
+++ src/func/matrix_create.c
@@ -1,15 +1,17 @@
 #include "../../includes/my.h"
 
-void __Create(my_matrix *A, unsigned int m, unsigned int n) {
+void create(my_matrix *A, unsigned int m, unsigned int n)
+{
+    unsigned int i;
+
     A->m = m;
     A->n = n;
-    A->arr = malloc(m * sizeof(double*));
+    A->arr = malloc(m * sizeof(double *));
     if (A->arr == NULL) {
         fprintf(stderr, "Memory allocation failed.\n");
         exit(1);
     }
-    unsigned int i;
-    for(i=0; i<m; i++) {
+    for (i = 0; i < m; i++) {
         A->arr[i] = calloc(n, sizeof(double));
         if (A->arr[i] == NULL) {
             fprintf(stderr, "Memory allocation failed.\n");
@@ -18,14 +20,17 @@ void __Create(my_matrix *A, unsigned int m, unsigned int n) {
     }
 }
 
-void my_matrix_create(unsigned int m, unsigned int n, const unsigned int count, ...) {
+void my_matrix_create(unsigned int m, unsigned int n, \
+    const unsigned int count, ...)
+{
+    unsigned int i;
+
     va_list args;
     va_start(args, count);
-    unsigned int i;
-    for(i=0; i<count; i++) {
+    for (i = 0; i < count; i++) {
         my_matrix *A = va_arg(args, my_matrix *);
         my_matrix_free(1, A);
-        __Create(A, m, n);
+        create(A, m, n);
     }
     va_end(args);
 }
