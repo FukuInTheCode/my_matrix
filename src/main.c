e82952066c94623f29a862e33655c7fdd820c530
FukuInTheCode <tomc.code@gmail.com>
2023-08-16 03:20:39 +0200

    everything should be fine

--- src/main.c
+++ src/main.c
@@ -14,9 +14,8 @@ int main(int argc, char* argv[])
     my_matrix D = {.m = 0, .n = 0};
     my_matrix_create(3, 3, 2, &A, &B);
     my_matrix_randint(1, 10, 2, &A, &B);
-    my_matrix_getsubmatrix(&A, 0, 0, &C);
-    my_matrix_inverse(&C, &D);
-    printf("det: %f\n", my_matrix_det(&C));
+    my_matrix_swapcol(&A, 0, 1, &C);
+    my_matrix_swaprow(&A, 0, 1, &D);
     my_matrix_print(4, &A, &B, &C, &D);
     my_matrix_free(4, &A, &B, &C, &D);
     return 0;
