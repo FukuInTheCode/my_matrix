535662707327593d76f70ad023b867c47a402a48
FukuInTheCode <tomc.code@gmail.com>
2023-08-16 18:21:28 +0200

    matrixh cs

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -21,14 +21,16 @@ void my_matrix_set(my_matrix_t *A, const unsigned int x, \
     const unsigned int y, const double n);
 void my_matrix_print(const unsigned int count, ...);
 void my_matrix_free(const unsigned int count, ...);
-void my_matrix_multiplybyscalar(my_matrix_t *A, double scalar, my_matrix_t *result);
+void my_matrix_multiplybyscalar(my_matrix_t *A, double scalar, \
+    my_matrix_t *result);
 void my_matrix_add(my_matrix_t *result, const unsigned int count, ...);
 void my_matrix_transpose(my_matrix_t *A, my_matrix_t* T);
 void my_matrix_getcolumn(my_matrix_t *A, unsigned int n, double result[]);
 double* my_matrix_getrow(my_matrix_t *A, unsigned int i);
 void my_matrix_product(my_matrix_t *result, const unsigned int count, ...);
 void my_matrix_copy(my_matrix_t *A, my_matrix_t *copy);
-void my_matrix_powerint(my_matrix_t *A, const unsigned int n, my_matrix_t *result);
+void my_matrix_powerint(my_matrix_t *A, const unsigned int n, \
+    my_matrix_t *result);
 void my_matrix_randint(const int minN, const int maxN,\
     const unsigned int count, ...);
 void my_matrix_randfloat(const double minN, const double maxN, \
@@ -39,11 +41,14 @@ void my_matrix_getsubmatrix(my_matrix_t *A, \
 void my_matrix_adjugate(my_matrix_t *A, my_matrix_t *result);
 void my_matrix_inverse(my_matrix_t *A, my_matrix_t *result);
 void my_matrix_addscalar(my_matrix_t *A, double scalar, my_matrix_t *result);
-void my_matrix_applyfunc(my_matrix_t *A, UnaryFunction func, my_matrix_t *result);
+void my_matrix_applyfunc(my_matrix_t *A, UnaryFunction func, \
+    my_matrix_t *result);
 double my_matrix_sum(my_matrix_t *A);
 void my_matrix_one(my_matrix_t *A, my_matrix_t *result);
-void my_matrix_addcol(my_matrix_t *A, const unsigned int n, my_matrix_t *result);
-void my_matrix_addrow(my_matrix_t *A, const unsigned int m, my_matrix_t *result);
+void my_matrix_addcol(my_matrix_t *A, const unsigned int n, \
+    my_matrix_t *result);
+void my_matrix_addrow(my_matrix_t *A, const unsigned int m, \
+    my_matrix_t *result);
 void my_matrix_concatcol(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B);
 void my_matrix_concatrow(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B);
 int my_matrix_equals(my_matrix_t *A, my_matrix_t *B);
@@ -65,6 +70,7 @@ double my_matrix_maxcol(my_matrix_t *A, const unsigned int n);
 double my_matrix_maxrow(my_matrix_t *A, const unsigned int m);
 double my_matrix_mincol(my_matrix_t *A, const unsigned int n);
 double my_matrix_minrow(my_matrix_t *A, const unsigned int m);
-void my_matrix_create_arr(unsigned int m, unsigned int n, my_matrix_t ***arr, uint8_t size);
+void my_matrix_create_arr(unsigned int m, unsigned int n, \
+    my_matrix_t ***arr, uint8_t size);
 void my_matrix_free_arr(my_matrix_t ***arr, uint8_t size);
 void my_matrix_print_arr(my_matrix_t ***arr, uint8_t size);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@ int main(int argc, char* argv[])
     srand(time(NULL));
     my_matrix_t **arr;
     my_matrix_create_arr(2, 2, &arr, 3);
-    my_matrix_identity(1, arr[1]);
+    my_matrix_randint(0, 10, 2, arr[1], arr[2]);
+    my_matrix_product(arr[0], 2, arr[2], arr[1]);
     my_matrix_print_arr(&arr, 3);
     my_matrix_free_arr(&arr, 3);
     return 0;
