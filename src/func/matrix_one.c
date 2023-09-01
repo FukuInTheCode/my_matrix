7ed1554479e290f877326c7c5632809d143fb492
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 17:40:16 +0200

    one 2 added

--- src/func/matrix_one.c
+++ src/func/matrix_one.c
@@ -10,3 +10,8 @@ void my_matrix_one(my_matrix_t *A, my_matrix_t *result)
 {
     my_matrix_applyfunc(A, setone, result);
 }
+
+void my_matrix_one_2(my_matrix_t *A)
+{
+    my_matrix_applyfunc_2(A, setone);
+}
