014b20f2bf202c18903fe741dd83dbda6cc2696f
FukuInTheCode <tomc.code@gmail.com>
2023-09-01 17:58:08 +0200

    recompile

--- src/main.c
+++ src/main.c
@@ -3,24 +3,5 @@
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
-
-    typedef struct {
-        my_matrix_t *arr;
-    } test_t;
-
-    my_matrix_t *arr;
-
-    test_t t;
-
-    my_matrix_create_array(&(t.arr), "B", 1, 2, 2);
-
-    // my_matrix_print(1, arr[0]);
-
-
-    my_matrix_print(1, &(t.arr[0]));
-    my_matrix_create(10, 10, 1, &(t.arr[0]));
-    my_matrix_print(1, &(t.arr[0]));
-    my_matrix_free_array(&arr, 1);
-
     return 0;
 }
