b379d59c2d2a83d397c7c8bb13349fd6101821bc
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:37:30 +0200

    changein lmax row

--- src/func/matrix_max_row.c
+++ src/func/matrix_max_row.c
@@ -1,16 +1,15 @@
 #include "../../includes/my.h"
 
-double my_matrix_maxrow(my_matrix_t *A, const unsigned int m)
+double my_matrix_maxrow(my_matrix_t *A, uint32_t const m)
 {
-    unsigned int i;
-
     if (m >= A->m) {
         fprintf(stderr, "Row index too high!\n");
         exit(1);
     }
     double max = A->arr[0][m];
-    for (i = 0; i < A->n; i++) {
-        if (max < A->arr[m][i]) max = A->arr[m][i];
+    for (uint32_t i = 0; i < A->n; i++) {
+        if (max < A->arr[m][i])
+            max = A->arr[m][i];
     }
     return max;
 }
