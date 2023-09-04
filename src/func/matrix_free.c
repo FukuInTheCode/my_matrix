19c9db7e521bd1502a24da40ed69b9298955b0e4
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:48:36 +0200

    chnage in the matrix free

--- src/func/matrix_free.c
+++ src/func/matrix_free.c
@@ -17,13 +17,11 @@ static int free_is_valid(my_matrix_t *A)
     return (A->m == 0 || A->n == 0 || A->arr == NULL) ? 84 : 0;
 }
 
-void my_matrix_free(const unsigned int count, ...)
+void my_matrix_free(uint32_t const count, ...)
 {
-    unsigned int i;
-
     va_list args;
     va_start(args, count);
-    for (i = 0; i < count; i++) {
+    for (uint32_t i = 0; i < count; i++) {
         my_matrix_t *A = va_arg(args, my_matrix_t *);
         if (free_is_valid(A) == 84) continue;
         my_free(A);
