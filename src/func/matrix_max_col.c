6417b644c308378d233d0b8d35b1f0830d9a2735
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:36:54 +0200

    chnagein maxcol

--- src/func/matrix_max_col.c
+++ src/func/matrix_max_col.c
@@ -1,16 +1,15 @@
 #include "../../includes/my.h"
 
-double my_matrix_maxcol(my_matrix_t *A, const unsigned int n)
+double my_matrix_maxcol(my_matrix_t *A, uint32_t const n)
 {
-    unsigned int i;
-
     if (n >= A->n) {
         fprintf(stderr, "Column index too high!\n");
         exit(1);
     }
     double max = A->arr[0][n];
-    for (i = 0; i < A->m; i++) {
-        if (max < A->arr[i][n]) max = A->arr[i][n];
+    for (uint32_t i = 0; i < A->m; i++) {
+        if (max < A->arr[i][n])
+            max = A->arr[i][n];
     }
     return max;
 }
