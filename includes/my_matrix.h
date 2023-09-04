a842c607f5b09eafb3183488bc4397221ea92803
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:49:17 +0200

    chnage in the header

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -35,7 +35,7 @@ void my_matrix_identity(uint32_t const count, ...);
 void my_matrix_set(my_matrix_t *A, uint32_t const x,\
                         uint32_t const y, double const  n);
 void my_matrix_print(uint32_t const count, ...);
-void my_matrix_free(const unsigned int count, ...);
+void my_matrix_free(uint32_t const count, ...);
 void my_matrix_multiplybyscalar(my_matrix_t *A, double scalar, \
     my_matrix_t *result);
 void my_matrix_add(my_matrix_t *result, const unsigned int count, ...);
