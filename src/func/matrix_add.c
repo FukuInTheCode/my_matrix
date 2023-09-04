2aa5e552b655d27a6dec883b8392a77560bf2314
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:55:53 +0200

    chnage in the add fun

--- src/func/matrix_add.c
+++ src/func/matrix_add.c
@@ -5,7 +5,7 @@ static my_bool_t add_is_valid(va_list args, uint32_t const count)
     my_matrix_t *A = va_arg(args, my_matrix_t *);
     uint32_t baseM = A->m;
     uint32_t baseN = A->n;
-    for (uint32_t i = 0; i < count-1; i++) {
+    for (uint32_t i = 0; i < count - 1; i++) {
         A = va_arg(args, my_matrix_t *);
         if ((A->m != baseM && A->m != 1) || (A->n != baseN && A->n != 1))
             return FALSE;
@@ -39,7 +39,7 @@ void my_matrix_add(my_matrix_t *result, uint32_t const count, ...)
     my_matrix_t *A = va_arg(args, my_matrix_t *);
     my_matrix_create(A->m, A->n, 1, result);
     my_matrix_copy(A, result);
-    for (uint32_t i = 0; i < (count-1); i++) {
+    for (uint32_t i = 0; i < (count - 1); i++) {
         A = va_arg(args, my_matrix_t *);
         add(result, A);
     }
