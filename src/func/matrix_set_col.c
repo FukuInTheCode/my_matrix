bf0aaff3824eca0b7df8a535bb4047ba53c98330
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:27:31 +0200

    chnagein set col

--- src/func/matrix_set_col.c
+++ src/func/matrix_set_col.c
@@ -1,10 +1,8 @@
 #include "../../includes/my.h"
 
-void my_matrix_setcol(my_matrix_t *A, const unsigned int n, const double x)
+void my_matrix_setcol(my_matrix_t *A, uint32_t const n, double const x)
 {
-    unsigned int i;
-
     if (n >= A->n) return;
-    for (i = 0; i < A->m; i++)
+    for (uint32_t i = 0; i < A->m; i++)
         A->arr[i][n] = x;
 }
