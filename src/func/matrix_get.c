f53073f2557230e321299d05fc31cb9c6995c0fe
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:11:49 +0200

    chnage oin submartrix

--- src/func/matrix_get.c
+++ src/func/matrix_get.c
@@ -12,19 +12,17 @@ void my_matrix_getcolumn(my_matrix_t *A, uint32_t n, double result[])
         result[i] = A->arr[i][n];
 }
 
-void my_matrix_getsubmatrix(my_matrix_t *A, const unsigned int m, \
-    const unsigned int n, my_matrix_t *result)
+void my_matrix_getsubmatrix(my_matrix_t *A, uint32_t const m, \
+    uint32_t const n, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
-    unsigned int i2 = 0;
-    unsigned int j2;
+    uint32_t i2 = 0;
 
     my_matrix_create(A->m - 1, A->n - 1, 1, result);
-    for (i = 0; i < A->m; i++) {
+    for (uint32_t i = 0; i < A->m; i++) {
         if (i == m) continue;
-        j2 = 0;
-        for (j = 0; j < A->n && j < n; j++) {
+        uint32_t j2 = 0;
+        uint32_t j = 0;
+        for (; j < A->n && j < n; j++) {
             result->arr[i2][j2] = A->arr[i][j];
             j2++;
         }
