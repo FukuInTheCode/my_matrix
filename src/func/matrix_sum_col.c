7aa28e217c9b9a9f5619227d73f4b7b6f3e65608
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:25:34 +0200

    change in sumcol

--- src/func/matrix_sum_col.c
+++ src/func/matrix_sum_col.c
@@ -2,14 +2,12 @@
 
 void my_matrix_sumcol(my_matrix_t *A, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
     double sum;
 
     my_matrix_create(1, A->n, 1, result);
-    for (i = 0; i < result->n; i++) {
+    for (uint32_t i = 0; i < result->n; i++) {
         sum = 0;
-        for (j = 0; j < A->m; j++)
+        for (uint32_t j = 0; j < A->m; j++)
             sum += A->arr[j][i];
         result->arr[0][i] = sum;
     }
