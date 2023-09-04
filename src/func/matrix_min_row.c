d366f3dc6d7868e527fe812dbd7d6b061eeda4c6
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:38:40 +0200

    chnagein min row

--- src/func/matrix_min_row.c
+++ src/func/matrix_min_row.c
@@ -1,16 +1,15 @@
 #include "../../includes/my.h"
 
-double my_matrix_minrow(my_matrix_t *A, const unsigned int m)
+double my_matrix_minrow(my_matrix_t *A, uint32_t const m)
 {
-    unsigned int i;
-
     if (m >= A->m) {
         fprintf(stderr, "Row index too high!\n");
         exit(1);
     }
     double min = A->arr[0][m];
-    for (i = 0; i < A->n; i++) {
-        if (min > A->arr[m][i]) min = A->arr[m][i];
+    for (uint32_t i = 0; i < A->n; i++) {
+        if (min > A->arr[m][i])
+            min = A->arr[m][i];
     }
     return min;
 }
