3fc1ad4db70540819b814c0aa12fccc63730e09b
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 22:42:24 +0200

    chnage in the header

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -31,7 +31,7 @@ typedef double (*temp_func)(double);
 
 void my_matrix_create(uint32_t m, uint32_t n, \
     uint32_t const count, ...);
-void my_matrix_identity(const unsigned int count, ...);
+void my_matrix_identity(uint32_t const count, ...);
 void my_matrix_set(my_matrix_t *A, const unsigned int x, \
     const unsigned int y, const double n);
 void my_matrix_print(const unsigned int count, ...);
