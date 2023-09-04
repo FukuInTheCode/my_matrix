80f44520b7db8e7f9ff1e448e99de5c1ca24d17f
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:26:06 +0200

    change in sum row

--- src/func/matrix_sum_row.c
+++ src/func/matrix_sum_row.c
@@ -2,14 +2,12 @@
 
 void my_matrix_sumrow(my_matrix_t *A, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
     double sum;
 
     my_matrix_create(A->m, 1, 1, result);
-    for (i = 0; i < result->m; i++) {
+    for (uint32_t i = 0; i < result->m; i++) {
         sum = 0;
-        for (j = 0; j < A->n; j++)
+        for (uint32_t j = 0; j < A->n; j++)
             sum += A->arr[i][j];
         result->arr[i][0] = sum;
     }
