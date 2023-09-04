e14f3d83ee1d96addeadd38bb718a1d5373a9802
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:02:41 +0200

    change matrix copy

--- src/func/matrix_copy.c
+++ src/func/matrix_copy.c
@@ -2,10 +2,9 @@
 
 void my_matrix_copy(my_matrix_t* A, my_matrix_t* copy)
 {
-    unsigned int i, j;
     my_matrix_create(A->m, A->n, 1, copy);
-    for (i = 0; i < A->m; i++) {
-        for (j = 0; j < A->n; j++) {
+    for (uint32_t i = 0; i < A->m; i++) {
+        for (uint32_t j = 0; j < A->n; j++) {
             my_matrix_set(copy, j, i, A->arr[i][j]);
         }
     }
