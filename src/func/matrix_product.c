2e9107577276142500eb73368f013748a883f3fb
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:02:10 +0200

    chnage in product func

--- src/func/matrix_product.c
+++ src/func/matrix_product.c
@@ -17,13 +17,11 @@ static void my_product(my_matrix_t *A, my_matrix_t *B, my_matrix_t *result)
 {
     if (A->n != B->m) return;
 
-    unsigned int i, j;
-
     my_matrix_create(A->m, B->n, 1, result);
 
-    for (i = 0; i < result->m; i++) {
+    for (uint32_t i = 0; i < result->m; i++) {
         double *row = my_matrix_getrow(A, i);
-        for (j = 0; j < result->n; j++) {
+        for (uint32_t j = 0; j < result->n; j++) {
             double column[B->m];
             my_matrix_getcolumn(B, j, column);
             my_matrix_set(result, j, i, my_dot_product(row, column, A->n));
