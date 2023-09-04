47918305f028f03ba82ff109e35efe44ca5f440f
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:15:36 +0200

    chnage in the apply func

--- src/func/matrix_apply.c
+++ src/func/matrix_apply.c
@@ -2,13 +2,10 @@
 
 void my_matrix_applyfunc(my_matrix_t *A, temp_func func, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
-
     my_matrix_copy(A, result);
 
-    for ( i = 0; i < result->m; i++) {
-        for (j = 0; j < result->n; j++) {
+    for (uint32_t i = 0; i < result->m; i++) {
+        for (uint32_t j = 0; j < result->n; j++) {
             my_matrix_set(result, j, i, func(result->arr[i][j]));
         }
     }
