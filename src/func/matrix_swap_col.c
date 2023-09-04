2a95c4e29a72fe727cc816d2a73802e4ae136597
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:24:44 +0200

    chnage in swap col

--- src/func/matrix_swap_col.c
+++ src/func/matrix_swap_col.c
@@ -1,14 +1,12 @@
 #include "../../includes./my.h"
 
-void my_matrix_swapcol(my_matrix_t *A, const unsigned int a, \
-    const unsigned int b, my_matrix_t *result)
+void my_matrix_swapcol(my_matrix_t *A, uint32_t const a, \
+    uint32_t const b, my_matrix_t *result)
 {
     if (a >= A->n || b >= A->n) return;
 
-    unsigned int i;
-
     my_matrix_copy(A, result);
-    for (i = 0; i < result->m; i++)
+    for (uint32_t i = 0; i < result->m; i++)
         swap(&(A->arr[i][a]), &(A->arr[i][b]));
 }
 
