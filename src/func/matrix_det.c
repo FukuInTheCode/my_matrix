bf5b81793f0397858c10c7d173a67eb10594ea11
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:10:06 +0200

    chnag in matrix det

--- src/func/matrix_det.c
+++ src/func/matrix_det.c
@@ -3,20 +3,19 @@
 double my_matrix_det(my_matrix_t *A)
 {
     double det = 0;
-    unsigned int i;
 
     if (A->m != A->n) {
         fprintf(stderr, "Matrix is not square matrix!");
         exit(1);
     } else if (A->m == 1) return A->arr[0][0];
 
-    for (i = 0; i < A->n; i++){
+    for (uint32_t i = 0; i < A->n; i++){
         double sign = my_power(-1.0, i);
 
-        my_matrix_t submatrix = {.m = 0, .n = 0};
+        MAT_DECLA(submatrix);
         my_matrix_getsubmatrix(A, 0, i, &submatrix);
         det += sign * A->arr[0][i] * my_matrix_det(&submatrix);
-        my_matrix_free(1, &submatrix);
+        MAT_FREE(submatrix);
     }
     return det;
 }
