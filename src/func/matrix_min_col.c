0b5703842bd745291718c1e943f6158b9af659c0
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:38:06 +0200

    chnage in min col

--- src/func/matrix_min_col.c
+++ src/func/matrix_min_col.c
@@ -1,16 +1,16 @@
 #include "../../includes/my.h"
 
-double my_matrix_mincol(my_matrix_t *A, const unsigned int n)
+double my_matrix_mincol(my_matrix_t *A, uint32_t const n)
 {
-    unsigned int i;
     if (n >= A->n) {
         fprintf(stderr, "Column index too high!\n");
         exit(1);
     }
     double min = A->arr[0][n];
 
-    for (i = 0; i < A->m; i++) {
-        if (min > A->arr[i][n]) min = A->arr[i][n];
+    for (uint32_t i = 0; i < A->m; i++) {
+        if (min > A->arr[i][n])
+            min = A->arr[i][n];
     }
     return min;
 }
