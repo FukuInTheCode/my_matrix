879eec91949e00c69518d3727ab0d6fbf6640a19
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:17:48 +0200

    change in the add col

--- src/func/matrix_add_col.c
+++ src/func/matrix_add_col.c
@@ -1,18 +1,15 @@
 #include "../../includes/my.h"
 
-void my_matrix_addcol(my_matrix_t *A, const unsigned int n, my_matrix_t *result)
+void my_matrix_addcol(my_matrix_t *A, uint32_t const n, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
-    unsigned int i2;
-    unsigned int j2 = 0;
+    uint32_t j2 = 0;
 
     if (n > A->n) return;
     my_matrix_create(A->m, A->n + 1, 1, result);
-    for (j = 0; j < result->n; j++) {
+    for (uint32_t j = 0; j < result->n; j++) {
         if (j == n) continue;
-        i2 = 0;
-        for (i = 0; i < result->m; i++) {
+        uint32_t i2 = 0;
+        for (uint32_t i = 0; i < result->m; i++) {
             result->arr[i][j] = A->arr[i2][j2];
             i2++;
         }
