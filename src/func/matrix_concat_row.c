bb1c765179ba95938256b127281e32f7d4176c88
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:20:26 +0200

    chnage in concat row

--- src/func/matrix_concat_row.c
+++ src/func/matrix_concat_row.c
@@ -2,16 +2,13 @@
 
 void my_matrix_concatrow(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B)
 {
-    unsigned int i;
-    unsigned int j;
-
     if (A->n != B->n) return;
     my_matrix_create(A->m + B->m, A->n, 1, result);
-    for (i = 0; i < result->n; i++) {
-        for (j = 0; j < A->m; j++) {
+    for (uint32_t i = 0; i < result->n; i++) {
+        for (uint32_t j = 0; j < A->m; j++) {
             result->arr[j][i] = A->arr[j][i];
         }
-        for (j = A->m; j < result->m; j++) {
+        for (uint32_t j = A->m; j < result->m; j++) {
             result->arr[j][i] = B->arr[j - A->m][i];
         }
     }
