26147888bd97a3e0226038af65ce2b97ff273e4a
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:31:39 +0200

    chhage in matrix max

--- src/func/matrix_max.c
+++ src/func/matrix_max.c
@@ -3,11 +3,9 @@
 double my_matrix_max(my_matrix_t *A)
 {
     double max = A->arr[0][0];
-    unsigned int i;
-    unsigned int j;
 
-    for (i = 0; i < A->m; i++) {
-        for (j = 0; j < A->n; j++)
+    for (uint32_t i = 0; i < A->m; i++) {
+        for (uint32_t j = 0; j < A->n; j++)
             max = max < A->arr[i][j] ? A->arr[i][j] : max;
     }
     return max;
