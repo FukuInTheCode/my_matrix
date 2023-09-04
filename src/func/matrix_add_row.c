d629e05c530d5a09c896f27867d7e394d9c248f1
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:19:02 +0200

    chnage in addrow func

--- src/func/matrix_add_row.c
+++ src/func/matrix_add_row.c
@@ -1,19 +1,16 @@
 #include "../../includes/my.h"
 
-void my_matrix_addrow(my_matrix_t *A, const unsigned int m, my_matrix_t *result)
+void my_matrix_addrow(my_matrix_t *A, uint32_t const m, my_matrix_t *result)
 {
-    unsigned int i;
-    unsigned int j;
-    unsigned int i2 = 0;
-    unsigned int j2;
+    uint32_t i2 = 0;
 
     if (m > A->m) return;
     my_matrix_create(A->m + 1, A->n, 1, result);
 
-    for (i = 0; i < result->m; i++) {
+    for (uint32_t i = 0; i < result->m; i++) {
         if (i == m) continue;
-        j2 = 0;
-        for (j = 0; j < result->n; j++) {
+        uint32_t j2 = 0;
+        for (uint32_t j = 0; j < result->n; j++) {
             result->arr[i][j] = A->arr[i2][j2];
             j2++;
         }
