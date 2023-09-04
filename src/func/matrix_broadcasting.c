d7b6e48db6977f6b0869936657e272a61eaf60ac
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:30:23 +0200

    chnage in broadcasting func

--- src/func/matrix_broadcasting.c
+++ src/func/matrix_broadcasting.c
@@ -1,25 +1,23 @@
 #include "../../includes/my.h"
 
-void my_matrix_broadcasting(my_matrix_t *A, unsigned int m, \
-    unsigned int n, my_matrix_t *result)
+void my_matrix_broadcasting(my_matrix_t *A, uint32_t m, \
+    uint32_t n, my_matrix_t *result)
 {
-    unsigned int i;
-
     if (A->m > 1 && m > A->m) m = A->m;
     if (A->n > 1 && n > A->n) n = A->n;
     my_matrix_create(m, n, 1, result);
     if (A->m == 1 && A->n == 1) {
-        my_matrix_t tmp = {.m = 0, .n = 0};
-        my_matrix_addscalar(result, A->arr[0][0], &tmp);
-        my_matrix_copy(&tmp, result);
-        my_matrix_free(1, &tmp);
+        my_matrix_addscalar_2(result, A->arr[0][0]);
         return;
     }
     if (A->m == 1 && A->n != 1)
-        for (i = 0; i < A->n; i++) my_matrix_setcol(result, i, A->arr[0][i]);
+        for (uint32_t i = 0; i < A->n; i++)
+            my_matrix_setcol(result, i, A->arr[0][i]);
     if (A->n == 1 && A->m != 1)
-        for (i = 0; i < A->m; i++) my_matrix_setrow(result, i, A->arr[i][0]);
-    if (A->m != 1 && A->n != 1) my_matrix_copy(A, result);
+        for (uint32_t i = 0; i < A->m; i++)
+            my_matrix_setrow(result, i, A->arr[i][0]);
+    if (A->m != 1 && A->n != 1)
+        my_matrix_copy(A, result);
 }
 
 void my_matrix_broadcasting_2(my_matrix_t *A, unsigned int m, \
