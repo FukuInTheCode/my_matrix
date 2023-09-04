a7d48071e3d543bcf12d59d82705e875f8bad003
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:20:00 +0200

    chnag concat col

--- src/func/matrix_concat_col.c
+++ src/func/matrix_concat_col.c
@@ -2,17 +2,14 @@
 
 void my_matrix_concatcol(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B)
 {
-    unsigned int i;
-    unsigned int j;
-
     if (A->m != B->m) return;
     my_matrix_create(A->m, A->n + B->n, 1, result);
 
-    for (i = 0; i < result->m; i++) {
-        for (j = 0; j < A->n; j++) {
+    for (uint32_t i = 0; i < result->m; i++) {
+        for (uint32_t j = 0; j < A->n; j++) {
             result->arr[i][j] = A->arr[i][j];
         }
-        for (j = A->n; j < result->n; j++) {
+        for (uint32_t j = A->n; j < result->n; j++) {
             result->arr[i][j] = B->arr[i][j - A->n];
         }
     }
