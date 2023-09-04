0f98778d34796e50d5b03804955a4e6b8988a40f
FukuInTheCode <tomc.code@gmail.com>
2023-09-04 23:00:08 +0200

    chnage in product func

--- src/func/matrix_product.c
+++ src/func/matrix_product.c
@@ -33,26 +33,25 @@ static void my_product(my_matrix_t *A, my_matrix_t *B, my_matrix_t *result)
     }
 }
 
-void my_matrix_product(my_matrix_t* result, const unsigned int count, ...)
+void my_matrix_product(my_matrix_t* result, uint32_t const count, ...)
 {
     va_list args;
     va_list args_copy;
     va_copy(args_copy, args);
     va_start(args_copy, count);
-    if (product_is_valid(args_copy, count) == 1) {
+    if (product_is_valid(args_copy, count) == FALSE) {
         va_end(args_copy);
         return;
     }
     va_end(args_copy);
     va_start(args, count);
-    my_matrix_t* A = va_arg(args, my_matrix_t*);
-    my_matrix_t copy = {.m = 0, .n = 0};
+    my_matrix_t *A = va_arg(args, my_matrix_t *);
+    MAT_DECLA(copy);
     my_matrix_copy(A, &copy);
-    unsigned int i;
-    for (i = 0; i < (count-1); i++) {
-        my_matrix_t* B = va_arg(args, my_matrix_t*);
+    for (uint32_t i = 0; i < (count - 1); i++) {
+        my_matrix_t *B = va_arg(args, my_matrix_t *);
         my_product(&copy, B, result);
         my_matrix_copy(result, &copy);
     }
-    my_matrix_free(1, &copy);
+    MAT_FREE(copy);
 }
