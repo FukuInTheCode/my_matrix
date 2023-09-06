723c18c14d90679c4dd1f48630efc3b45cc46850
FukuInTheCode <tomc.code@gmail.com>
2023-09-06 22:16:00 +0200

    math  h changrs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@ int main(int argc, char* argv[])
     MAT_DECLA(A);
     my_matrix_create(1, 1, 1, &A);
     my_matrix_print(1, &A);
-    my_matrix_create(2, 1, 1, &A);
+    my_matrix_create(2, 2, 1, &A);
+    my_matrix_identity(1, &A);
     my_matrix_print(1, &A);
     MAT_FREE(A);
     return 0;
