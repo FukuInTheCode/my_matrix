4d6d88fb7ad933955f8e94c9f3de42fd42a4de87
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 00:59:15 +0200

    copy array cs

--- src/main.c
+++ src/main.c
@@ -3,12 +3,13 @@
 int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    MAT_DECLA(A);
-    my_matrix_create(1, 1, 1, &A);
-    my_matrix_print(1, &A);
-    my_matrix_create(2, 2, 1, &A);
-    my_matrix_identity(1, &A);
-    my_matrix_print(1, &A);
-    MAT_FREE(A);
+    my_matrix_t *arr;
+
+    my_matrix_t *cpy;
+
+    my_matrix_create_array(&arr, "Bite", 3, 1, 1, 2, 2, 3, 3);
+
+    
+
     return 0;
 }
