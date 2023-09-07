8aee0431ef3a37a4531a10236d89dabf88cb5f2c
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 16:33:59 +0200

    add a row index find

--- src/func/matrix_find_index.c
+++ src/func/matrix_find_index.c
@@ -8,3 +8,12 @@ uint32_t my_matrix_find_col_index(my_matrix_t *A, uint32_t m,  double x)
     }
     return A->n;
 }
+
+uint32_t my_matrix_find_row_index(my_matrix_t *A, uint32_t n,  double x)
+{
+    for (uint32_t i = 0; i < A->m; ++i) {
+        if (A->arr[i][n] == x)
+            return i;
+    }
+    return A->m;
+}
