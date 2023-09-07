569193a1a45e530b0beb897d6dd1d18ca06168ac
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 01:41:01 +0200

    add meshgrid 2

--- src/func/matrix_meshgrid.c
+++ src/func/matrix_meshgrid.c
@@ -8,3 +8,14 @@ void my_matrix_meshgrid(my_matrix_t *x, my_matrix_t *y,\
     my_matrix_transpose(y, yy);
     my_matrix_broadcasting_2(yy, y->n, x->n);
 }
+
+void my_matrix_meshgrid_2(my_matrix_t *x, my_matrix_t *y)
+{
+    MAT_DECLA(cpy1);
+    MAT_DECLA(cpy2);
+    my_matrix_copy(x, &cpy1);
+    my_matrix_copy(y, &cpy2);
+    my_matrix_meshgrid(&cpy1, &cpy2, x, y);
+    MAT_FREE(cpy1);
+    MAT_FREE(cpy2);
+}
