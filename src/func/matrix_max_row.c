7e6c8637b3187c065c7ab1ec739ab7a7cc40f38b
FukuInTheCode <tomc.code@gmail.com>
2023-09-07 23:58:45 +0200

    BUG FIX

--- src/func/matrix_max_row.c
+++ src/func/matrix_max_row.c
@@ -6,7 +6,7 @@ double my_matrix_maxrow(my_matrix_t *A, uint32_t const m)
         fprintf(stderr, "Row index too high!\n");
         exit(1);
     }
-    double max = A->arr[0][m];
+    double max = A->arr[m][0];
     for (uint32_t i = 0; i < A->n; i++) {
         if (max < A->arr[m][i])
             max = A->arr[m][i];
