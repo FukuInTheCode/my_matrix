174197d6858df8fa628364c8ba60b4935b964f4d
FukuInTheCode <tomc.code@gmail.com>
2023-09-08 00:45:08 +0200

    bug fix

--- src/func/matrix_linspace.c
+++ src/func/matrix_linspace.c
@@ -1,9 +1,10 @@
 #include "../../includes/my.h"
 
-void my_matrix_linspace(my_matrix_t *m, double start, double stop, uint32_t num)
+void my_matrix_linspace(my_matrix_t *m, double start,\
+                                double stop, uint32_t num)
 {
     my_matrix_create(1, num, 1, m);
-    double step = (double)num / (stop - start);
+    double step = (double)num / (stop - start + 1);
     for (uint32_t i = 0; i < num; ++i)
         my_matrix_set(m, 0, i, i * step + start);
 }
--- src/main.c
+++ src/main.c
@@ -2,20 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-    srand(time(NULL));
-    my_matrix_t *arr;
+    srand(69);
 
-    my_matrix_t *cpy;
+    MAT_DECLA(A);
 
-    my_matrix_create_array(&arr, "Bite", 3, 1, 1, 2, 2, 3, 3);
-    my_matrix_randfloat(-30, 30, 1, &(arr[2]));
-    my_matrix_set(&(arr[2]), 0, 0, -10);
+    my_matrix_linspace(&A, -10, 10, 21);
 
-    my_matrix_print_array(&arr, 3);
+    my_matrix_transpose_2(&A);
 
-    my_matrix_copy_array(&arr, &cpy, 3, "zizi");
+    MAT_PRINT(A);
 
-    my_matrix_print_array(&cpy, 3);
+    MAT_FREE(A);
 
     return 0;
 }
