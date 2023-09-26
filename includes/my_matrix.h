f0515ad5d69f12173564e96cf52d4aa724f02d8b
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:10:15 +0200

    add add row implementation to header

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -121,6 +121,34 @@ void my_matrix_meshgrid(my_matrix_t *x, my_matrix_t *y,\
 void my_matrix_meshgrid_2(my_matrix_t *x, my_matrix_t *y);
 void my_matrix_to_array(my_matrix_t *A, double **arr);
 
+#ifdef MY_MAT_IMPLEMENTATION
+
+void my_matrix_addcol(my_matrix_t *A, uint32_t const n, my_matrix_t *result)
+{
+    uint32_t j2 = 0;
+
+    if (n > A->n) return;
+    my_matrix_create(A->m, A->n + 1, 1, result);
+    for (uint32_t j = 0; j < result->n; j++) {
+        if (j == n) continue;
+        uint32_t i2 = 0;
+        for (uint32_t i = 0; i < result->m; i++) {
+            result->arr[i][j] = A->arr[i2][j2];
+            i2++;
+        }
+        j2++;
+    }
+}
+
+void my_matrix_addcol_2(my_matrix_t *A, uint32_t const n)
+{
+    MAT_DECLA(cpy);
+    my_matrix_copy(A, &cpy);
+    my_matrix_addcol(&cpy, n, A);
+    MAT_FREE(cpy);
+}
+#endif
+
 #ifdef MATRIX_INIT_STR
 
 static inline __attribute__((always_inline)) char *init_str(char *str, int i)
