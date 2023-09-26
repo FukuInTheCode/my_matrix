21c6e50d0cbc4d3fd9d81bf924e1b94b45933212
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:14:42 +0200

    changes

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -283,6 +283,20 @@ void my_matrix_concatcol(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B)
     }
 }
 
+void my_matrix_concatrow(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B)
+{
+    if (A->n != B->n) return;
+    my_matrix_create(A->m + B->m, A->n, 1, result);
+    for (uint32_t i = 0; i < result->n; i++) {
+        for (uint32_t j = 0; j < A->m; j++) {
+            result->arr[j][i] = A->arr[j][i];
+        }
+        for (uint32_t j = A->m; j < result->m; j++) {
+            result->arr[j][i] = B->arr[j - A->m][i];
+        }
+    }
+}
+
 #endif
 
 #ifdef MATRIX_INIT_STR
