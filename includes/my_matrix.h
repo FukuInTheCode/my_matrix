d685135d993ff140b42dfc5f2ab2f4d4606373d8
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:12:19 +0200

    changes in h

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -238,6 +238,35 @@ void my_matrix_applyfunc_2(my_matrix_t *A, temp_func func)
     my_matrix_applyfunc(&cpy, func, A);
     MAT_FREE(cpy);
 }
+
+void my_matrix_broadcasting(my_matrix_t *A, uint32_t m, \
+    uint32_t n, my_matrix_t *result)
+{
+    if (A->m > 1 && m > A->m) m = A->m;
+    if (A->n > 1 && n > A->n) n = A->n;
+    my_matrix_create(m, n, 1, result);
+    if (A->m == 1 && A->n == 1) {
+        my_matrix_addscalar_2(result, A->arr[0][0]);
+        return;
+    }
+    if (A->m == 1 && A->n != 1)
+        for (uint32_t i = 0; i < A->n; i++)
+            my_matrix_setcol(result, i, A->arr[0][i]);
+    if (A->n == 1 && A->m != 1)
+        for (uint32_t i = 0; i < A->m; i++)
+            my_matrix_setrow(result, i, A->arr[i][0]);
+    if (A->m != 1 && A->n != 1)
+        my_matrix_copy(A, result);
+}
+
+void my_matrix_broadcasting_2(my_matrix_t *A, uint32_t m, \
+    uint32_t n)
+{
+    MAT_DECLA(cpy);
+    my_matrix_copy(A, &cpy);
+    my_matrix_broadcasting(&cpy, m, n, A);
+    MAT_FREE(cpy);
+}
 #endif
 
 #ifdef MATRIX_INIT_STR
