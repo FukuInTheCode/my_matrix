29af082d8bbae3962287b57e9f7c593e9b3c948a
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:15:07 +0200

    changes

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -297,6 +297,29 @@ void my_matrix_concatrow(my_matrix_t *result, my_matrix_t *A, my_matrix_t *B)
     }
 }
 
+void my_matrix_copy(my_matrix_t *A, my_matrix_t *copy)
+{
+    my_matrix_create(A->m, A->n, 1, copy);
+    for (uint32_t i = 0; i < A->m; i++) {
+        for (uint32_t j = 0; j < A->n; j++) {
+            my_matrix_set(copy, i, j, A->arr[i][j]);
+        }
+    }
+}
+
+void my_matrix_copy_array(my_matrix_t **arr, my_matrix_t **copy,\
+                                    uint32_t size, char *common_name)
+{
+    *copy = malloc(size * sizeof(my_matrix_t));
+    check_alloc(*copy);
+    for (uint32_t i = 0; i < size; ++i) {
+        (*copy)[i].m = 0;
+        (*copy)[i].n = 0;
+        my_matrix_copy(&((*arr)[i]), &((*copy)[i]));
+        (*copy)[i].name = init_str(common_name, i);
+    }
+}
+
 #endif
 
 #ifdef MATRIX_INIT_STR
