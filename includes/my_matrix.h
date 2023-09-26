8e186f5e8315aae9b1a1b74093fe0fd512b53908
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:11:12 +0200

    add row implementation

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -147,6 +147,32 @@ void my_matrix_addcol_2(my_matrix_t *A, uint32_t const n)
     my_matrix_addcol(&cpy, n, A);
     MAT_FREE(cpy);
 }
+
+void my_matrix_addrow(my_matrix_t *A, uint32_t const m, my_matrix_t *result)
+{
+    uint32_t i2 = 0;
+
+    if (m > A->m) return;
+    my_matrix_create(A->m + 1, A->n, 1, result);
+
+    for (uint32_t i = 0; i < result->m; i++) {
+        if (i == m) continue;
+        uint32_t j2 = 0;
+        for (uint32_t j = 0; j < result->n; j++) {
+            result->arr[i][j] = A->arr[i2][j2];
+            j2++;
+        }
+        i2++;
+    }
+}
+
+void my_matrix_addrow_2(my_matrix_t *A, uint32_t const n)
+{
+    MAT_DECLA(cpy);
+    my_matrix_copy(A, &cpy);
+    my_matrix_addrow(&cpy, n, A);
+    MAT_FREE(cpy);
+}
 #endif
 
 #ifdef MATRIX_INIT_STR
