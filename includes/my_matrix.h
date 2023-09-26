b9f072a89b3c045079b6e965233676de38f1f974
FukuInTheCode <tomc.code@gmail.com>
2023-09-26 22:17:50 +0200

    changes in h

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -385,6 +385,20 @@ double my_matrix_det(my_matrix_t *A)
     return det;
 }
 
+my_bool_t my_matrix_equals(my_matrix_t *A, my_matrix_t *B)
+{
+    unsigned int i;
+
+    if (A->m != B->m || A->n != B->n)
+        return FALSE;
+
+    for (i = 0; i < A->m * A->n; i++) {
+        if (A->arr[i / A->n][i % A->n] != B->arr[i / A->n][i % A->n])
+            return my_false;
+    }
+    return my_true;
+}
+
 #endif
 
 #ifdef MATRIX_INIT_STR
