295f87170f8cda27b2f42f093cc23c0734318b15
FukuInTheCode <tomc.code@gmail.com>
2023-09-27 21:44:06 +0200

    changes in h

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -399,6 +399,25 @@ my_bool_t my_matrix_equals(my_matrix_t *A, my_matrix_t *B)
     return my_true;
 }
 
+uint32_t my_matrix_find_col_index(my_matrix_t *A, uint32_t m,  double x)
+{
+    for (uint32_t i = 0; i < A->n; ++i) {
+        if (A->arr[m][i] == x)
+            return i;
+    }
+    return A->n;
+}
+
+uint32_t my_matrix_find_row_index(my_matrix_t *A, uint32_t n,  double x)
+{
+    for (uint32_t i = 0; i < A->m; ++i) {
+        if (A->arr[i][n] == x)
+            return i;
+    }
+    return A->m;
+}
+
+
 #endif
 
 #ifdef MATRIX_INIT_STR
