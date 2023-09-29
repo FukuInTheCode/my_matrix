f1caf284b13f2eb87a62a8053ea363f0d5fdbecf
FukuInTheCode <tomc.code@gmail.com>
2023-09-29 21:29:29 +0200

    change in h

--- includes/my_matrix.h
+++ includes/my_matrix.h
@@ -417,6 +417,46 @@ uint32_t my_matrix_find_row_index(my_matrix_t *A, uint32_t n,  double x)
     return A->m;
 }
 
+static void my_free(my_matrix_t *A)
+{
+    for (uint32_t i = 0; i < A->m; i++) {
+        free(A->arr[i]);
+    }
+    free(A->arr);
+    A->arr = NULL;
+    A->m = 0;
+    A->n = 0;
+}
+
+static my_bool_t free_is_valid(my_matrix_t *A)
+{
+    return (A->m == 0 || A->n == 0 || A->arr == NULL) ? my_false: my_true;
+}
+
+void my_matrix_free(uint32_t const count, ...)
+{
+    va_list args;
+    va_start(args, count);
+    for (uint32_t i = 0; i < count; i++) {
+        my_matrix_t *A = va_arg(args, my_matrix_t *);
+        if (free_is_valid(A) == my_false) continue;
+        my_free(A);
+    }
+    va_end(args);
+}
+
+void my_matrix_free_array(my_matrix_t **arr, uint32_t size)
+{
+    for (uint32_t i = 0; i < size; i++) {
+        my_matrix_t A = (*arr)[i];
+        if (free_is_valid(&A) == my_false) continue;
+        my_free(&A);
+        free(A.name);
+        A.name = NULL;
+    }
+    free(*arr);
+}
+
 
 #endif
 
